a8cb245060fccbc91720953e12e50f96ac5c3787
nisitin <seiya110307@icloud.com>
2024-02-12 18:21:22 +0900

    feat: 依存関係のバージョンアップ

---
+++ WereWolfGameClient/View/Components/TextField/CustomTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomInput.swift
+//  WereWolfGameClient
+//
+//  Created by 西聖也 on 2024/02/12.
+//
+
+import SwiftUI
+
+struct CustomInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomInput()
+}
